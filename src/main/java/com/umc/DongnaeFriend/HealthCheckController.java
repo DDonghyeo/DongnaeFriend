c7bcebb0d88fcc8f1fafa5c6fb6b46a2ada5cc74
JungYoonShin <sju08227@naver.com>
2023-07-13 17:07:41 +0900

    :fire: develop : HealthCheckController 불필요한 코드 삭제

--- src/main/java/com/umc/DongnaeFriend/HealthCheckController.java
+++ src/main/java/com/umc/DongnaeFriend/HealthCheckController.java
@@ -1,6 +1,4 @@
 package com.umc.DongnaeFriend;
-import com.umc.DongnaeFriend.domain.user.entity.User;
-import com.umc.DongnaeFriend.domain.user.entity.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,14 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class HealthCheckController {
-    private final UserRepository userRepository;
-
-    @GetMapping("")
-    public String hi() {
-        User user = new User();
-        userRepository.save(user);
-        return "hih????";
-    }
 
     @GetMapping("/health")
     public String healthCheck() {
