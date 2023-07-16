18ad0a2c1f642ad2f7171aa534376d610ab66b46
JungYoonShin <sju08227@naver.com>
2023-07-16 17:59:26 +0900

    :green_heart: Ci: nginx 관련 파일 ec2 주소 수정

--- src/main/java/com/umc/DongnaeFriend/HealthCheckController.java
+++ src/main/java/com/umc/DongnaeFriend/HealthCheckController.java
@@ -1,7 +1,4 @@
 package com.umc.DongnaeFriend;
-import com.umc.DongnaeFriend.domain.user.entity.User;
-import com.umc.DongnaeFriend.domain.user.entity.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +7,6 @@ public class HealthCheckController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "health check ok!!!!!!";
+        return "health check ok!!!!!";
     }
 }
