e2ce8d5893008ae0bf00938deb1d528383173259
sungone <20200999@sungshin.ac.kr>
2023-07-31 14:18:13 +0900

    :white_check_mark: Feat #57 : [가계부 공유] 공감 기능 수정

--- src/main/java/com/umc/DongnaeFriend/domain/account/sharing/repository/SharingSympathyRepository.java
+++ src/main/java/com/umc/DongnaeFriend/domain/account/sharing/repository/SharingSympathyRepository.java
@@ -7,9 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface SharingSympathyRepository extends JpaRepository<SharingSympathy, Long> {
     @Query("SELECT u FROM User u WHERE u.id = :user_id")
     User findByUserId(@Param("user_id") Long user_id);
     @Query("SELECT sb FROM SharingBoard sb WHERE sb.id = :sharing_board_id")
     SharingBoard findBySharingBoardId(@Param("sharing_board_id") Long sharing_board_id);
+
+    int countAllBySharingBoardId(Long sharing_board_id);
+    int countAllByUserId(Long userId);
+    List<SharingSympathy> findByUser_Id(long user_id);
 }
