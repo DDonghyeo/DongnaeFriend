2b36fe97a9326afc708cc7f72612ac13809edd9d
JungYoonShin <sju08227@naver.com>
2023-07-31 22:16:44 +0900

    배포 확인

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,19 +1,33 @@
-#!/usr/bin/env bash
+version: 0.0
+os: linux
 
-PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+files:
+  - source:  /
+    destination: /home/ubuntu/app
+    overwrite: yes
 
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+permissions:
+  - object: /
+    pattern: "**"
+    owner: ubuntu
+    group: ubuntu
 
-TIME_NOW=$(date +%c)
-
-# 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-
-# 프로세스가 켜져 있으면 종료
-if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
-else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 $CURRENT_PID
-fi
+hooks:
+  ApplicationStart:
+    - location: scripts/run_new_was.sh
+      timeout: 180
+      runas: ubuntu
+    - location: scripts/health_check.sh
+      timeout: 180
+      runas: ubuntu
+    - location: scripts/switch.sh
+      timeout: 180
+      runas: ubuntu
+#  AfterInstall:
+#    - location: scripts/stop.sh
+#      timeout: 60
+#      runas: ubuntu
+#  ApplicationStart:
+#    - location: scripts/start.sh
+#      timeout: 60
+#      runas: ubuntu
