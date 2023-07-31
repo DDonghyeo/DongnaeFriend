4868b24045b1056e8b80e8e7749b304d9c3eb0fe
JungYoonShin <sju08227@naver.com>
2023-07-31 22:29:33 +0900

    무중단 배포 확인

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,33 +1,19 @@
-version: 0.0
-os: linux
+#!/usr/bin/env bash
 
-files:
-  - source:  /
-    destination: /home/ubuntu/app
-    overwrite: yes
+PROJECT_ROOT="/home/ubuntu/app"
+JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
-permissions:
-  - object: /
-    pattern: "**"
-    owner: ubuntu
-    group: ubuntu
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-hooks:
-  ApplicationStart:
-    - location: scripts/run_new_was.sh
-      timeout: 180
-      runas: ubuntu
-    - location: scripts/health_check.sh
-      timeout: 180
-      runas: ubuntu
-    - location: scripts/switch.sh
-      timeout: 180
-      runas: ubuntu
-#  AfterInstall:
-#    - location: scripts/stop.sh
-#      timeout: 60
-#      runas: ubuntu
-#  ApplicationStart:
-#    - location: scripts/start.sh
-#      timeout: 60
-#      runas: ubuntu
+TIME_NOW=$(date +%c)
+
+# 현재 구동 중인 애플리케이션 pid 확인
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+
+# 프로세스가 켜져 있으면 종료
+if [ -z $CURRENT_PID ]; then
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+else
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
+fi
