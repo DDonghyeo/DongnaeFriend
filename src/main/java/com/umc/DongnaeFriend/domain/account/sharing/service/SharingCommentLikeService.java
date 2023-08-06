f1c56b9818660e4053b39913756ef5dc648928d7
sungone <20200999@sungshin.ac.kr>
2023-08-07 00:50:54 +0900

    :white_check_mark: Feat #66 : [가계부 공유] 댓글 좋아요 기능 수정

--- src/main/java/com/umc/DongnaeFriend/domain/account/sharing/service/SharingCommentLikeService.java
+++ src/main/java/com/umc/DongnaeFriend/domain/account/sharing/service/SharingCommentLikeService.java
@@ -3,6 +3,7 @@ package com.umc.DongnaeFriend.domain.account.sharing.service;
 import com.umc.DongnaeFriend.domain.account.sharing.entity.SharingComment;
 import com.umc.DongnaeFriend.domain.account.sharing.entity.SharingCommentLike;
 import com.umc.DongnaeFriend.domain.account.sharing.repository.SharingCommentLikeRepository;
+import com.umc.DongnaeFriend.domain.dongnae.entity.DongnaeCommentLike;
 import com.umc.DongnaeFriend.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,13 +20,22 @@ public class SharingCommentLikeService {
         // 댓글 가져오기
         SharingComment sharingComment = sharingCommentLikeRepository.findByCommentId(commentId);
 
-        SharingCommentLike sharingCommentLike = SharingCommentLike.builder()
-                .user(user)
-                .sharingComment(sharingComment)
-                .build();
+        // 좋아요 유무 확인하기
+        SharingCommentLike sharingCommentLikeExist = sharingCommentLikeRepository.findByCommentId(sharingComment);
 
-        sharingCommentLikeRepository.save(sharingCommentLike);
+        if (sharingCommentLikeExist == null) {
+            SharingCommentLike sharingCommentLike = SharingCommentLike.builder()
+                    .user(user)
+                    .sharingComment(sharingComment)
+                    .build();
 
-        return "가계부 공유 댓글 좋아요 성공";
+            sharingCommentLikeRepository.save(sharingCommentLike);
+
+            return "가계부 공유 댓글 좋아요 성공";
+        }
+
+        sharingCommentLikeRepository.delete(sharingCommentLikeExist);
+
+        return "가계부 공유 댓글 좋아요 삭제 성공";
     }
 }
