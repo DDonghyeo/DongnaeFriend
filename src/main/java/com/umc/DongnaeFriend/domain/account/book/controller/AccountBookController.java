837a41365f4d4bb0f7830091237ab93319765951
soogoori <soobin0209@naver.com>
2023-08-20 16:57:23 +0900

    ✨ Fix #63 : 가계부 조회 수정

--- src/main/java/com/umc/DongnaeFriend/domain/account/book/controller/AccountBookController.java
+++ src/main/java/com/umc/DongnaeFriend/domain/account/book/controller/AccountBookController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
@@ -18,17 +19,17 @@ public class AccountBookController {
     private final AccountBookService accountBookService;
 
     @GetMapping("/budget")
-    public ResponseEntity<AccountBookDto.BudgetResponse> getBudget(@RequestParam(value = "year") Integer year,
-                                                                   @RequestParam(value = "month")
+    public ResponseEntity<AccountBookDto.BudgetResponse> getBudget(@Valid @RequestParam(value = "year") Integer year,
+                                                                   @Valid @RequestParam(value = "month")
                                                                   @Max(value = 12, message = "12월까지만 입력해주세요") int month){
         return ResponseEntity.status(HttpStatus.OK).body(accountBookService.getBudget(year, month));
     }
 
     @PostMapping("/budget")
-    public ResponseEntity<?> createBudget(@RequestParam(value = "year") Integer year,
-                             @RequestParam(value = "month")
+    public ResponseEntity<?> createBudget(@Valid @RequestParam(value = "year") Integer year,
+                                          @Valid @RequestParam(value = "month")
                                             @Max(value = 12, message = "12월까지만 입력해주세요") int month,
-                             @RequestParam(value = "amount")
+                                          @Valid @RequestParam(value = "amount")
                                               @Min(value =0, message = "예산은 0원 이상이어야 합니다") long budget){
         accountBookService.createBudget(year, month, budget);
 
