7e09eba35242f18714bfbd285bec903a058ca57b
soogoori <soobin0209@naver.com>
2023-08-20 20:50:55 +0900

    ✨ Fix #63

--- src/main/java/com/umc/DongnaeFriend/domain/account/book/repository/accountBook/AccountBookRepositoryCustom.java
+++ src/main/java/com/umc/DongnaeFriend/domain/account/book/repository/accountBook/AccountBookRepositoryCustom.java
@@ -2,10 +2,11 @@ package com.umc.DongnaeFriend.domain.account.book.repository.accountBook;
 
 
 import com.umc.DongnaeFriend.domain.account.book.dto.Expense;
+import com.umc.DongnaeFriend.domain.user.entity.User;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface AccountBookRepositoryCustom {
-    List<Expense> getAccountBook(Integer year, Integer month);
+    List<Expense> getAccountBook(Integer year, Integer month, User user);
 }
