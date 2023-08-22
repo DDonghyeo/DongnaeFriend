d9c60ea764688c476d4cf8bc27db8bcc58b46fbf
DDonghyeo <gsu9045000@gmail.com>
2023-08-22 21:45:18 +0900

    Fix : contents dto

---
+++ src/main/java/com/umc/DongnaeFriend/global/dto/ContentsDto.java
@@ -0,0 +1,13 @@
+package com.umc.DongnaeFriend.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class ContentsDto {
+
+    List<?> contents;
+}
